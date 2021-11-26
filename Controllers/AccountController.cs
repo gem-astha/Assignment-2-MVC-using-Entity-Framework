5c8cb3a229cb35dd9b1ecef1a20e7a450f118072
gem-astha <92977563+gem-astha@users.noreply.github.com>
2021-11-26 15:54:17 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAss.Models;
+using System.Web.Security;
+
+namespace MVCAss.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Members model)
+        {
+            using (var context =new OfficeEntities())
+            {
+                bool isValid = context.Users1.Any(x=>x.UserName==model.UserName&&x.Password==model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employee1");
+                }
+                ModelState.AddModelError("", "Invalid Username and Password");
+                return View();
+            }
+            
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(Users1 model)
+        {
+            using(var context=new OfficeEntities())
+            {
+                context.Users1.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/Members.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAss.Models
+{
+    public class Members
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Password{ get; set; }
+    }
+}
